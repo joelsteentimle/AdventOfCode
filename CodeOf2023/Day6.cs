61832402fb5337bf1a941258826b75a8678d9cf2
joel steen timle <nordenjolt@gmail.com>
2023-12-06 07:45:10 +0100

    Day6

---
+++ CodeOf2023/Day6.cs
@@ -0,0 +1,41 @@
+﻿namespace AoC2023;
+
+public class Day6
+{
+    public readonly List<(long time, long distance)> Races;
+
+    public Day6(IList<string> input, bool isReal = false)
+    {
+        string[] distances;
+        string[] times;
+        if (isReal)
+        {
+            times = new[] { input.First()["Time: ".Length ..].Replace(" ", "") };
+            distances = new[] { input[1]["Distance: ".Length ..].Replace(" ", "") };
+        }
+        else
+        {
+            times = input.First()["Time: ".Length ..]
+                .Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+
+            distances = input[1]["Distance: ".Length ..]
+                .Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        Races = times.Zip(distances, (t, d) => (Convert.ToInt64(t), Convert.ToInt64(d))).ToList();
+    }
+
+    public static int CalculateRace((long time, long distance) race)
+    {
+        var winnings = 0;
+        for (long speed = 0; speed < race.time; speed++)
+            if (CalculateDistance(race.time, speed) > race.distance)
+                winnings++;
+
+        return winnings;
+    }
+
+    private static long CalculateDistance(long totalTime, long speed) => speed * (totalTime - speed);
+
+    public int TotalRaceWinnings() => Races.Aggregate(1, (current, race) => current * CalculateRace(race));
+}
---
+++ Testof2023/Day6Tests.cs
@@ -0,0 +1,55 @@
+﻿using AoC2023;
+
+namespace TestOf2023;
+
+public class Day6Tests
+{
+    [Test]
+    public void CanGetTimesAndDistance()
+    {
+        var d6 = new Day6("Day6".ReadTestLines());
+        Assert.That(d6.Races, Is.EquivalentTo(new List<(int, int)> { (7, 9), (15, 40), (30, 200) }));
+    }
+
+    [Test]
+    public void DoFirstRace()
+    {
+        var d6 = new Day6("Day6".ReadTestLines());
+        Assert.That(Day6.CalculateRace((7, 9)), Is.EqualTo(4));
+    }
+
+    [Test]
+    public void Part1Test()
+    {
+        var d6 = new Day6("Day6".ReadTestLines());
+        Assert.That(d6.TotalRaceWinnings(), Is.EqualTo(288));
+    }
+
+    [Test]
+    public void Part1()
+    {
+        var d6 = new Day6("Day6".ReadRealLines());
+        Assert.That(d6.TotalRaceWinnings(), Is.EqualTo(1108800));
+    }
+
+    [Test]
+    public void Part2CanReadRace()
+    {
+        var d6 = new Day6("Day6".ReadTestLines(), true);
+        Assert.That(d6.Races, Is.EquivalentTo(new List<(int, int)> { (71530, 940200) }));
+    }
+
+    [Test]
+    public void Part2Test()
+    {
+        var d6 = new Day6("Day6".ReadTestLines(), true);
+        Assert.That(d6.TotalRaceWinnings(), Is.EqualTo(71503));
+    }
+
+    [Test]
+    public void Part2()
+    {
+        var d6 = new Day6("Day6".ReadRealLines(), true);
+        Assert.That(d6.TotalRaceWinnings(), Is.EqualTo(36919753));
+    }
+}
