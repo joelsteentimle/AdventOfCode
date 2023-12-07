58ec63d151b19b7d3487895326fb92965ff0451e
Joel Steen Timle <joel.steen-timle@acorntechnology.se>
2023-12-07 20:19:52 +0100

    Day 7

---
+++ CodeOf2023/StringHelper.cs
@@ -0,0 +1,9 @@
+﻿namespace AoC2023;
+
+public static class StringHelper
+{
+    public static IList<string> SplitAndTrim(this string input, params  char[] separator)
+    {
+        return input.Split(separator, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+    }
+}
