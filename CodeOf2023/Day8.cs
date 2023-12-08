16fe1b2d173a8a5f29bbc67d5a53514e5e7df863
Joel Steen Timle <joel.steen-timle@acorntechnology.se>
2023-12-08 08:41:08 +0100

    day 8 start

---
+++ CodeOf2023/Day8.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualBasic;
+
+namespace AoC2023;
+
+public class Day8
+{
+    public Day8(string[] input)
+    {
+        MoveInstructions = input.First().ToCharArray();
+        foreach (var line in input[2 ..])
+        {
+            var node = new DesertNode(line);
+            Nodes[node.Name] = node;
+        }
+    }
+
+    public Dictionary<string, DesertNode> Nodes { get; } = [];
+
+    public char[] MoveInstructions { get; set; }
+    public struct DesertNode
+    {
+        public string Right { get; }
+        public string Name { get; }
+        public string Left { get; }
+
+        public DesertNode(string line)
+        {
+            Name = line[..3];
+            Left = line[7..10];
+            Right = line[12..15];
+        }
+    }
+}
+
---
+++ Testof2023/Day8Tests.cs
@@ -0,0 +1,29 @@
+﻿using AoC2023;
+
+namespace TestOf2023;
+
+public class Day8Tests
+{
+    [Test]
+    public void CanReadInput()
+    {
+        var d8 = new Day8("Day8".ReadTestLines().ToArray());
+    Assert.Multiple(() =>
+    {
+        Assert.That(d8.Nodes["BBB"].Left, Is.EqualTo("DDD"));
+        Assert.That(d8.Nodes["BBB"].Right, Is.EqualTo("EEE"));
+    });
+    }
+
+    [Test]
+    public void CanCreateNode()
+    {
+        var node = new Day8.DesertNode("AAA = (BBB, CCC)");
+            Assert.Multiple(() =>
+            {
+                Assert.That(node.Name,Is.EqualTo("AAA"));
+                Assert.That(node.Left,Is.EqualTo("BBB"));
+                Assert.That(node.Right,Is.EqualTo("CCC"));
+            });
+    }
+}
