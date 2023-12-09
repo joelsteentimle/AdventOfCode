2c7718a43f5cabdad0f7298fa418fec245962d2a
joel steen timle <nordenjolt@gmail.com>
2023-12-09 16:04:08 +0100

    Day9

---
+++ CodeOf2023/Day9.cs
@@ -0,0 +1,47 @@
+﻿namespace AoC2023;
+
+public class Day9
+{
+    public Day9(IList<string> lines)
+    {
+        OriginalSeries = lines.Select(l => l.SplitAndTrim(' ').Select(n => Convert.ToInt32(n)).ToList()).ToList();
+    }
+
+    public ( long left,long right) SumNexts() => OriginalSeries.Select(GetNextValue).Aggregate((0L,0L), (v1, v2) =>(v1.Item1+v2.left,v1.Item2+v2.right));
+
+    public (long left,long right ) GetNextValue(List<int> values)
+    {
+        if (values.Count == 0 || values.All(v => v == 0))
+            return (0,0);
+        
+        Stack<List<int>> valueReduce = [];
+        
+        var previus = values;
+        var current = new List<int>();
+
+        do
+        {
+            current = [];
+            valueReduce.Push(previus);
+            for (var i = 0; i+1 < previus.Count; i++)
+            {
+                current.Add(previus[i+1] - previus[i]);
+            }
+
+            previus = current;
+        } while (current.Any(c => c != 0));
+
+        long rightNumber = 0;
+        long leftNumber = 0;
+        while (valueReduce.Any())
+        {
+            var list = valueReduce.Pop();
+            rightNumber += list.Last() ;
+            leftNumber = list.First() - leftNumber;
+        }
+
+        return (leftNumber,rightNumber);
+    }
+
+    public List<List<int>> OriginalSeries { get; set; }
+}
---
+++ Testof2023/Day9Tests.cs
@@ -0,0 +1,63 @@
+﻿using AoC2023;
+
+namespace TestOf2023;
+
+public class Day9Tests
+{
+    [Test]
+    public void CanRead()
+    {
+        var d9 = new Day9("Day9".ReadTestLines());
+    }
+
+    [Test]
+    public void CanCountLine()
+    {
+        var d9 = new Day9("Day9".ReadTestLines());
+        Assert.Multiple(() =>
+        {
+            Assert.That(d9.GetNextValue([0, 3, 6, 9, 12, 15]).right, Is.EqualTo(18));
+            Assert.That(d9.GetNextValue([1, 3, 6, 10, 15, 21]).right, Is.EqualTo(28));
+            Assert.That(d9.GetNextValue([10, 13, 16, 21, 30, 45]).right, Is.EqualTo(68));
+
+        });
+    }
+
+    [Test]
+    public void CanCountLeft()
+    {
+        var d9 = new Day9("Day9".ReadTestLines());
+        Assert.That(d9.GetNextValue([10, 13, 16, 21, 30, 45]).left, Is.EqualTo(5));
+    }
+
+
+    [Test]
+    public void Part1Test()
+    {
+        var d9 = new Day9("Day9".ReadTestLines());
+
+        Assert.That(d9.SumNexts().right, Is.EqualTo(114));
+    }
+
+    [Test]
+    public void Part1()
+    {
+        var d9 = new Day9("Day9".ReadRealLines());
+
+        Assert.That(d9.SumNexts().right, Is.EqualTo(114));
+    }
+
+    [Test]
+    public void Part2Test()
+    {
+        var d9 = new Day9("Day9".ReadTestLines());
+        Assert.That(d9.SumNexts().left, Is.EqualTo(2));
+    }
+
+    [Test]
+    public void Part2()
+    {
+        var d9 = new Day9("Day9".ReadRealLines());
+        Assert.That(d9.SumNexts().left, Is.EqualTo(114));
+    }
+}
