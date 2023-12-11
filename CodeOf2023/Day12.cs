52d9163a70cfaae2e4b0ced542d656d5ede8f508
joel steen timle <nordenjolt@gmail.com>
2023-12-11 21:36:42 +0100

    Day12 skeleton

---
+++ CodeOf2023/Day12.cs
@@ -0,0 +1,8 @@
+﻿namespace AoC2023;
+
+public class Day12
+{
+    public Day12(IList<string> readTestLines)
+    {
+    }
+}
---
+++ Testof2023/Day12Tests.cs
@@ -0,0 +1,14 @@
+﻿using AoC2023;
+
+namespace TestOf2023;
+
+public class Day12Tests
+{
+    [Test]
+    public void CanReadInput()
+    {
+        var d12 = new Day12("Day12".ReadTestLines());
+
+        // Assert.That(d12.field, Is.EqualTo());
+    }
+}
