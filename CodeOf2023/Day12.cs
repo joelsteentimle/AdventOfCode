391266bf122c09a498291d4bb64cf56f3922c0da
Joel Steen Timle <joel.steen-timle@acorntechnology.se>
2023-12-12 16:41:02 +0100

    Day 12 part 1

--- CodeOf2023/Day12.cs
+++ CodeOf2023/Day12.cs
@@ -2,7 +2,80 @@
 
 public class Day12
 {
-    public Day12(IList<string> readTestLines)
+    public Day12(IList<string> lines)
     {
+        Fields = lines.Select(l => new Field(l)).ToList();
+    }
+    
+    public int GetSum => Fields.Select(f => f.TotalCombinations()).Sum();
+
+    public IList<Field> Fields { get; set; }
+
+    public class Field
+    {
+        public Field(string line)
+        {
+            var patternAndSprings = line.SplitAndTrim(' ');
+            Pattern = patternAndSprings[0];
+            Springs = patternAndSprings[1].SplitAndTrim(',').Select(n => Convert.ToInt32(n)).ToList();
+            Combinations = new int?[Pattern.Length, Springs.Count];
+        }
+
+        public int?[,] Combinations { get; set; }
+
+        public List<int> Springs { get; set; }
+
+
+        public string Pattern { get; set; }
+
+        public int TotalCombinations() => GetCombinations(0, 0);
+        
+        public int GetCombinations(int startInPattern, int springNumber)
+        {
+            if (springNumber >= Springs.Count || startInPattern >= Pattern.Length)
+                return 0;
+
+            var preCalculated = Combinations[startInPattern, springNumber];
+
+            if (preCalculated is not null)
+                return preCalculated.Value;
+
+            var springSize = Springs[springNumber];
+            
+            if (startInPattern + springSize > Pattern.Length)
+            {
+                Combinations[startInPattern, springNumber] = 0;
+                return 0;
+            }
+  
+            var totalMatches = 0;
+
+            if (MatchStart(startInPattern, springNumber))
+            {
+                if (springNumber + 1 == Springs.Count
+                    && Pattern[(startInPattern + springSize) ..].All(c => c!= '#'))
+                {
+                    totalMatches += 1;
+                }
+                else if(startInPattern + springSize < Pattern.Length &&
+                        Pattern[startInPattern + springSize] is '.' or '?')
+                {
+                    totalMatches += GetCombinations(startInPattern + springSize + 1, springNumber + 1);
+                }
+            }
+
+            if (Pattern[startInPattern] is '.' or '?')
+                totalMatches += GetCombinations(startInPattern + 1, springNumber);
+
+            Combinations[startInPattern, springNumber] = totalMatches;
+            return totalMatches;
+        }
+
+
+        public bool MatchStart(int startInPattern, int springNumber)
+        {
+            var springSize = Springs[springNumber];
+            return Pattern[startInPattern .. (startInPattern + springSize)].All(c => c is '#' or '?');
+        }
     }
 }
--- Testof2023/Day12Tests.cs
+++ Testof2023/Day12Tests.cs
@@ -8,7 +8,59 @@ public class Day12Tests
     public void CanReadInput()
     {
         var d12 = new Day12("Day12".ReadTestLines());
+        Assert.That(d12.Fields[0].Pattern, Is.EqualTo("???.###"));
+        Assert.That(d12.Fields[0].Springs, Is.EqualTo(new[]{ 1,1,3}));
+    }
 
-        // Assert.That(d12.field, Is.EqualTo());
+    [Test]
+    public void CanMatchPattern()
+    {
+        var d12f = new Day12.Field("?.???## 1,1,4");
+        Assert.That(d12f.MatchStart(3, 1));
+    }
+    
+    [Test]
+    public void CanFindForOneLine()
+    {
+        Assert.Multiple(() =>
+        {
+            Assert.That(LineVersions("???.### 1,1,3"), Is.EqualTo(1));
+            Assert.That(LineVersions(".??..??...?##. 1,1,3"), Is.EqualTo(4));
+            Assert.That(LineVersions("?###???????? 3,2,1"), Is.EqualTo(10));
+            Assert.That(LineVersions("???#?? 3"), Is.EqualTo(3));
+            Assert.That(LineVersions(".?##?????#?? 3,3"), Is.EqualTo(6));
+        });
+    }  
+    private static long LineVersions(string line) => new Day12.Field(line).TotalCombinations();
+
+    [Test]
+    public void TestAllRows()
+    {
+        var d12 = new Day12("Day12".ReadTestLines());
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(d12.Fields[0].TotalCombinations(), Is.EqualTo(1));        
+            Assert.That(d12.Fields[1].TotalCombinations(), Is.EqualTo(4));        
+            Assert.That(d12.Fields[2].TotalCombinations(), Is.EqualTo(1));        
+            Assert.That(d12.Fields[3].TotalCombinations(), Is.EqualTo(1));        
+            Assert.That(d12.Fields[4].TotalCombinations(), Is.EqualTo(4));         
+            Assert.That(d12.Fields[5].TotalCombinations(), Is.EqualTo(10));        
+
+        });
+    }   
+   
+    [Test]
+    public void Part1Test()
+    {
+        var d12 = new Day12("Day12".ReadTestLines());
+        Assert.That(d12.GetSum, Is.EqualTo(21));
+    }
+    
+    [Test]
+    public void Part1()
+    {
+        var d12 = new Day12("Day12".ReadRealLines());
+        Assert.That(d12.GetSum, Is.EqualTo(7195));
     }
 }
