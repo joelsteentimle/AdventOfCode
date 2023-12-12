84b09123a19e3e0e695412ea83093f350e9bd3ab
joel steen timle <nordenjolt@gmail.com>
2023-12-12 23:49:16 +0100

    Day 13

---
+++ CodeOf2023/Day13.cs
@@ -0,0 +1,133 @@
+﻿namespace AoC2023;
+
+public class Day13
+{
+    public Day13(IList<string> lines)
+    {
+       Fields =  lines.Aggregate(new List<List<string>> { new() }, (list, line) =>
+            {
+                if (line.Trim() == string.Empty)
+                {
+                    list.Add([]);
+                }
+                else
+                {
+                    list.Last().Add(line);
+                }
+                return list;
+            }
+        )
+           .Select(bl => new Field(bl))
+        .ToList();
+    }
+
+    public int Sum => Fields.Sum(f => (f.HFold ?? 0) * 100 + (f.VFold ?? 0));
+    public int SmudgeSum => Fields.Sum(f => (f.HSmudgeFold ?? 0) * 100 + (f.VSmudgeFold ?? 0));
+    
+
+    public int Result { get; set; } = 123456;
+    
+    public List< Field > Fields{ get; set; }
+
+    public class Field
+    {
+        public Field(List<string> lines)
+        {
+            IsRock = new bool[lines.First().Length, lines.Count];
+
+            for (int y = 0; y < lines.Count; y++)
+            {
+                for (int x = 0; x < lines[y].Length; x++)
+                {
+                    IsRock[x, y] = lines[y][x] == '#';
+                }
+            }
+            
+            for (int x = 1; x < IsRock.GetLength(0); x++)
+            {
+                (bool current, bool smudge)  = CheckIfVMirror(x);
+                if (current)
+                {
+                    VFold = x;
+                }
+
+                if (smudge)
+                {
+                    VSmudgeFold = x;
+                }
+            }
+            
+            for (int y = 1; y < IsRock.GetLength(1); y++)
+            {
+                (bool current, bool smudge)  = CheckIfHMirror(y);
+                if (current)
+                {
+                    HFold = y;
+                    // break;
+                }
+
+                if (smudge)
+                {
+                    HSmudgeFold = y;
+                }
+            }
+        }
+
+        private (bool current, bool smudge) CheckIfVMirror(int i)
+        {
+            var right = i;
+            var left = i - 1;
+            int smudges = 0;
+            
+            while (left >= 0 && right < IsRock.GetLength(0))
+            {
+                for (var y = 0; y < IsRock.GetLength(1); y++)
+                {
+                    if (IsRock[right, y] != IsRock[left, y])
+                    {
+                        smudges++;
+                        if(smudges >1)
+                            return (false,false);
+                    }
+                }
+
+                left--;
+                right++;
+            }
+
+            return (smudges ==0, smudges == 1);
+        }
+        private (bool current, bool smudge) CheckIfHMirror(int i)
+        {
+            var upper = i;
+            var lower = i - 1;
+            var smudges = 0;
+            
+            while (lower >= 0 && upper < IsRock.GetLength(1))
+            {
+                for (var x = 0; x < IsRock.GetLength(0); x++)
+                {
+                    if (IsRock[x, lower] != IsRock[x, upper])
+                    {
+                        smudges++;
+                        if(smudges >1)
+                            return (false,false);
+                    }
+                }
+
+                upper++;
+                lower--;
+            }
+
+            return (smudges ==0, smudges == 1);
+
+        }
+
+        private bool[,] IsRock;
+
+        public int? VFold { get; set; }
+        public int? HFold { get; set; }
+        public int? VSmudgeFold { get; set; }
+        public int? HSmudgeFold { get; set; }
+    }
+}
---
+++ Testof2023/Day13Tests.cs
@@ -0,0 +1,72 @@
+﻿using AoC2023;
+
+namespace TestOf2023;
+
+public class Day13Tests
+{
+    [Test]
+    public void CanGetResult()
+    {
+        var d13 = GetTest();
+        Assert.That(d13.Result, Is.EqualTo(5555));
+    }
+
+    [Test]
+    public void GetCorrectNumberOfFields()
+    {
+        Assert.That(GetTest().Fields.Count, Is.EqualTo(2));
+    }
+
+    [Test]
+    public void CanFindVFoldInField()
+    {
+        var F1 = GetTest().Fields[0];
+        Assert.That(F1.VFold, Is.EqualTo(5));
+        Assert.That(F1.HFold, Is.Null);
+    }
+    [Test]
+    public void CanFindHFoldInField()
+    {
+        var F2 = GetTest().Fields[1];
+        Assert.That(F2.HFold, Is.EqualTo(4));
+        Assert.That(F2.VFold, Is.Null);
+    }
+
+    [Test]
+    public void Part1Test()
+    {
+        Assert.That(GetTest().Sum, Is.EqualTo(405));
+    }
+
+    [Test]
+    public void Part1()
+    {
+        Assert.That(GetReal().Sum, Is.EqualTo(33975));
+    }
+    
+    [Test]
+    public void Part2Test()
+    {
+        Assert.That(GetTest().SmudgeSum, Is.EqualTo(400));
+    }
+
+
+    [Test]
+    public void Part2()
+    {
+        Assert.That(GetReal().SmudgeSum, Is.EqualTo(29083));
+    }
+    
+
+    private static Day13 GetTest()
+    {
+        var d13 = new Day13("Day13".ReadTestLines());
+        return d13;
+    }
+    
+    private static Day13 GetReal()
+    {
+        var d13 = new Day13("Day13".ReadRealLines());
+        return d13;
+    }
+}
