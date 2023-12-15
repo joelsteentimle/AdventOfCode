e0a428750cc40bfcc795f82f1d15bf4a6d0367b9
Joel Steen Timle <joel.steen-timle@acorntechnology.se>
2023-12-15 08:51:11 +0100

    Day15

--- CodeOf2023/Day15.cs
+++ CodeOf2023/Day15.cs
@@ -1,15 +1,72 @@
-﻿namespace AoC2023;
+﻿using Open.Collections.NonGeneric;
+
+namespace AoC2023;
 
 public class Day15(List<string> lines)
 {
-    public char[,] Map { get; set; } = CalculateMap(lines);
+    public IList<string> Groups { get; } = lines.First().SplitAndTrim(',');
 
-    private static char[,] CalculateMap(List<string> list)
+    private static Dictionary<int, List<Lens>> ParseBoxes(string row)
     {
-        var initialMap = new char[list[0].Length, list.Count];
-        for (var y = 0; y < list.Count; y++)
-        for (var x = 0; x < list[y].Length; x++)
-            initialMap[x, y] = list[y][x];
-        return initialMap;
+        Dictionary<int, List<Lens>> result = [];
+        var boxStrings = row.SplitAndTrim(',').ToList();
+
+        foreach (var boxString in boxStrings)
+        {
+            var isAdd = boxString.Contains('=');
+            var nameAndNumber = boxString.SplitAndTrim('=', '-');
+            var key = nameAndNumber[0];
+            var hash = Hash(key);
+            if (isAdd)
+            {
+                var number = nameAndNumber[1].ToInt32();
+                var lenses = result.GetOrAdd(hash, new List<Lens>());
+                var existingLens = lenses.Find(l => l.Key == key);
+                if (existingLens is null)
+                    lenses.Add(new Lens(key, number));
+                else
+                    existingLens.Number = number;
+            }
+            else
+            {
+                if (result.TryGetValue(hash, out var box))
+                {
+                    var existingLens = box.Find(l => l.Key == key);
+                    if (existingLens is not null)
+                        box.Remove(existingLens);
+                }
+            }
+        }
+
+        return result;
     }
+
+    private Dictionary<int, List<Lens>> Boxes { get; } = ParseBoxes(lines.First());
+
+    private record Lens(string Key, int Number)
+    {
+        public int Number { get; set; } = Number;
+    }
+
+    public static int Hash(string message) => message.Aggregate(0, (r, c) => (r + Convert.ToInt32(c)) * 17 % 256);
+
+    public int HashSum => Groups.Select(Hash).Sum();
+
+    public int LensSum()
+    {
+        var sum = 0;
+
+        foreach (var key in Boxes.Keys)
+        {
+            var boxSum = 0;
+            var lenses = Boxes[key];
+            for (var i = 0; i < lenses.Count; i++) boxSum += (i + 1) * lenses[i].Number;
+
+            sum += boxSum * (key + 1);
+
+        }
+
+        return sum;
+    }
+
 }
--- Testof2023/Day15Tests.cs
+++ Testof2023/Day15Tests.cs
@@ -5,11 +5,30 @@ namespace TestOf2023;
 public class Day15Tests : DayTests
 {
     [Test]
-    public void CanGetRealMap() => Assert.That(RealInstance.Map, Has.Length.AtLeast(50));
+    public void CanGetRealMap() => Assert.That(RealInstance.Groups, Has.Count.AtLeast(50));
 
     [Test]
-    public void CanGetTestMap() => Assert.That(GetTestInstance().Map, Has.Length.AtLeast(5));
+    public void CanGetTestMap() => Assert.That(GetTestInstance().Groups[2], Is.EqualTo("qp=3"));
+
+    [Test]
+    public void CanHash() => Assert.That(Day15.Hash("HASH"), Is.EqualTo(52));
+
+    [Test]
+    public void VerifyHash() => Assert.That(Day15.Hash("rn"), Is.EqualTo(0));
+
+    [Test]
+    public void CanSumHash() => Assert.That(GetTestInstance().HashSum, Is.EqualTo(1320));
+
+    [Test]
+    public void Part1() => Assert.That(RealInstance.HashSum, Is.EqualTo(504449));
+
+    [Test]
+    public void Part2Test() => Assert.That(GetTestInstance().LensSum(), Is.EqualTo(145));
+
+    [Test]
+    public void Part2() => Assert.That(RealInstance.LensSum(), Is.EqualTo(262044));
 
     private Day15 RealInstance => new(GetRealLines());
+
     private Day15 GetTestInstance(string suffix = "") => new(GetTestLines(suffix));
 }
