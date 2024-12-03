82461453aab4f25ad8db07ff1ab0a23fbdb42ad6
Joel Steen Timle <joel.steentimle@twinfinity.com>
2024-12-03 14:06:43 +0100

    missing initial condition on part 2, day02

---
+++ AoC2024/Day02.cs
@@ -0,0 +1,43 @@
+﻿namespace AoC2024;
+
+public class Day02
+{
+    public Day02(List<string> lines, int tolerance)
+    {
+        foreach (var line in lines)
+        {
+            BreakToOuter:
+            var stairs = line.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            var previous = stairs[0];
+            var sign = Math.Sign(stairs[1] - stairs[0]);
+            var dangerous = 0;
+
+            foreach (var stair in stairs[1..])
+            {
+                var abs = Math.Abs(stair - previous);
+                var localSign = Math.Sign(stair - previous);
+
+                if (abs > 3 || abs < 1 ||  localSign != sign)
+                {
+                    dangerous++;
+                }
+                else
+                {
+                    previous = stair;
+                }
+            }
+
+            if(dangerous>tolerance)
+            {
+                Unsafe.Add(stairs);
+            }else
+            {
+                Safe.Add(stairs);
+            }
+        }
+    }
+
+    public List<List<int>> Safe { get;  } = new();
+    public List<List<int>> Unsafe { get; } = new();
+}
---
+++ TestOf2024/Day02Tests.cs
@@ -0,0 +1,35 @@
+using AoC2024;
+using SupportCode;
+
+namespace TestOf2026;
+
+public class Day02Tests
+{
+    [Test]
+    public void Test1()
+    {
+        var d2 = new Day02("Day02".ReadTestLines(),0);
+        Assert.That(d2.Safe.Count, Is.EqualTo(2));
+    }
+
+    [Test]
+    public void Real1()
+    {
+        var d2 = new Day02("Day02".ReadRealLines(),0);
+        Assert.That(d2.Safe.Count, Is.EqualTo(306));
+    }
+
+    [Test]
+    public void Test2()
+    {
+        var d2 = new Day02("Day02".ReadTestLines(),1);
+        Assert.That(d2.Safe.Count, Is.EqualTo(4));
+    }
+
+    [Test]
+    public void Real2()
+    {
+        var d2 = new Day02("Day02".ReadRealLines(),1);
+        Assert.That(d2.Safe.Count, Is.EqualTo(340));
+    }
+}
