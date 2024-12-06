8bd6788d1a52f28bf7e307a9462b8cf72e4d66df
Joel Steen Timle <joelsteentimle@gmail.com>
2024-12-06 18:18:43 +0100

    Day06

--- AoC2024/Day06.cs
+++ AoC2024/Day06.cs
@@ -9,13 +9,13 @@ public class Day06
         Visited
     };
 
-    private (int, int) GuardPosition = (0, 0);
-    private (int,int) GuardHeading = (0, 0);
+    private (int y, int x) GuardPosition = (0, 0);
+    private (int dy, int dx) GuardHeading = (0, 0);
     private Place[,] Field;
 
     public Day06(List<string> input)
     {
-        Field = new Place[input.Count,input[0].Length];
+        Field = new Place[input.Count, input[0].Length];
 
         for (var y = 0; y < input.Count; y++)
         {
@@ -59,13 +59,78 @@ public class Day06
         }
     }
 
-    private void MoveGuard()
-    {
-
-    }
-
     public int GuardVisitedSquares()
     {
-        return 0;
+        while (TryMoveGuard()) ;
+
+        var sum = 0;
+        foreach (var positions in Field)
+        {
+            if (positions == Place.Visited)
+                sum++;
+        }
+
+        return sum;
     }
+
+
+    private bool TryMoveGuard()
+    {
+        var nextSquare = Peek();
+        if (IsOutOfBound(nextSquare))
+        {
+            return false;
+        }
+
+        if (IsMovable(nextSquare))
+
+        {
+            GuardPosition = nextSquare;
+            Field[nextSquare.y, nextSquare.x] = Place.Visited;
+            return true;
+        }
+        else
+        {
+            var newY = GuardHeading.dx switch
+            {
+                1 => 1,
+                -1 => -1,
+                _ => 0
+            };
+            var newX = GuardHeading.dy switch
+            {
+                1 => -1,
+                -1 => 1,
+                _ => 0
+            };
+            GuardHeading = (newY, newX);
+            return true;
+        }
+
+    }
+
+    private bool IsOutOfBound((int, int ) nextSquare)
+    {
+        var (y, x) = nextSquare;
+        if (y < 0 || y >= Field.GetLength(0))
+            return true;
+        if (x < 0 || x >= Field.GetLength(1))
+            return true;
+        return false;
+    }
+
+    private bool IsMovable((int y, int x) nextSquare)
+    {
+
+        var (y, x) = nextSquare;
+        if (Field[y, x] != Place.Visited &&
+            Field[y, x] != Place.Open)
+            return false;
+
+        return true;
+    }
+
+    private (int y, int x) Peek() =>
+        (GuardPosition.y + GuardHeading.dy, GuardPosition.x + GuardHeading.dx);
+
 }
--- TestOf2024/Day06Tests.cs
+++ TestOf2024/Day06Tests.cs
@@ -9,15 +9,13 @@ public class Day06Tests
     public void Test1()
     {
         var d6 = new Day06("Day06".ReadTestLines());
-        Assert.That(d6.GuardVisitedSquares(), Is.EqualTo(143));
+        Assert.That(d6.GuardVisitedSquares(), Is.EqualTo(41));
     }
 
     [Test]
     public void Real1()
     {
         var d6 = new Day06("Day06".ReadRealLines());
-        Assert.That(d6.GuardVisitedSquares(), Is.EqualTo(143));
-
+        Assert.That(d6.GuardVisitedSquares(), Is.EqualTo(4696));
     }
-
 }
