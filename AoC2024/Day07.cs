6f6d434ce2e5cf9960691f51f7865285b3b0c3a7
Joel Steen Timle <joelsteentimle@gmail.com>
2024-12-08 01:17:51 +0100

    some more days

---
+++ AoC2024/Day07.cs
@@ -0,0 +1,58 @@
+namespace AoC2024;
+
+public class Day07
+{
+    private List<List<long>> numberLines = [];
+
+    public Day07(List<string> input)
+    {
+        foreach (var equation in input)
+        {
+            var numbers = equation
+                .Replace(":", "")
+                .Split(' ')
+                .Select(long.Parse)
+                .ToList();
+
+            numberLines.Add(numbers);
+        }
+    }
+
+    public long SumThatCan(bool withConcat = false)
+    {
+        var sum = 0L;
+        foreach (var numbers in numberLines)
+        {
+            var targetSum = numbers[0];
+            var parts = numbers[2..];
+
+            HashSet<long> currentNums = [numbers[1]];
+
+            foreach (var num in parts)
+            {
+                var currentNumList = currentNums.ToList();
+                currentNums = [];
+                foreach (var prev in currentNumList)
+                {
+                    var adds = prev + num;
+                    if (adds <= targetSum)
+                        currentNums.Add(adds);
+
+                    var muls = prev * num;
+                    if (muls <= targetSum)
+                        currentNums.Add(muls);
+
+                    if (withConcat &&
+                        long.TryParse($"{prev}{num}", out var concat)
+                        && concat <= targetSum)
+                        currentNums.Add(concat);
+                }
+            }
+
+            if (currentNums.Contains(targetSum))
+                sum += targetSum;
+        }
+
+        return sum;
+    }
+}
