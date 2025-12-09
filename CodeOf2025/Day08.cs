f356afb9c9603081ff251ccc5e6145aed63ac926
joel <nordenjolt@gmail.com>
2025-12-09 21:53:15 +0100

    Day 8

--- CodeOf2025/Day08.cs
+++ CodeOf2025/Day08.cs
@@ -22,24 +22,89 @@ public class Day08
 
     private record NodeDistance(float distance, int n1i, int n2i);
 
-    public long NodeThreeProduct(int numberOfPairs)
+    public long Part1(int numberOfPairs)
     {
         Dictionary<int, int> connectedToCluster = [];
-        Dictionary<int, HashSet<int>>  clusterContains  = [];
+        Dictionary<int, HashSet<int>> clusterContains = [];
 
         var orderedDistances = FillDistances();
-         var index = 0;
+        var index = 0;
+        var clusterKey = 0;
 
-        for (var pairs = 0; pairs < numberOfPairs; index++)
+        for (var connectionMade = 0; connectionMade < numberOfPairs; index++)
         {
             var (_, n1i, n2i) = orderedDistances[index];
             int? c1 = null;
             int? c2 = null;
 
-            if(connectedToCluster.TryGetValue(n1i, out var n1C))
-               c1 = n1C;
+            if (connectedToCluster.TryGetValue(n1i, out var n1C))
+                c1 = n1C;
             if (connectedToCluster.TryGetValue(n2i, out var n2C))
                 c2 = n2C;
+
+            connectionMade++;
+
+            if (c1.HasValue && c2.HasValue && c1.Value == c2.Value)
+                continue;
+
+
+            if (c1.HasValue)
+            {
+                if (c2.HasValue)
+                {
+                    foreach (var i in clusterContains[c2.Value])
+                    {
+                        connectedToCluster[i] = c1.Value;
+                        clusterContains[c1.Value].Add(i);
+                    }
+                    clusterContains.Remove(c2.Value);
+                }
+
+                connectedToCluster[n2i] = c1.Value;
+                clusterContains[c1.Value].Add(n2i);
+            }
+            else if (c2.HasValue)
+            {
+                connectedToCluster[n1i] = c2.Value;
+                clusterContains[c2.Value].Add(n1i);
+            }
+            else
+            {
+                var newClusterKey = clusterKey++;
+                connectedToCluster.Add(n1i, newClusterKey);
+                connectedToCluster.Add(n2i, newClusterKey);
+                clusterContains[newClusterKey] = [n1i, n2i];
+
+            }
+        }
+
+        var threeLargestClusters = clusterContains.Values.Select(hs => hs.Count).OrderDescending()
+            .Take(3);
+        var threeLargestProduct = threeLargestClusters.             Aggregate((a, b) => a * b);
+
+        return threeLargestProduct;
+    }
+
+    public long Part2()
+    {
+        Dictionary<int, int> connectedToCluster = [];
+        Dictionary<int, HashSet<int>> clusterContains = [];
+
+        var orderedDistances = FillDistances();
+
+        var clusterKey = 0;
+
+        for (   var index = 0; ; index++)
+        {
+            var (_, n1i, n2i) = orderedDistances[index];
+            int? c1 = null;
+            int? c2 = null;
+
+            if (connectedToCluster.TryGetValue(n1i, out var n1C))
+                c1 = n1C;
+            if (connectedToCluster.TryGetValue(n2i, out var n2C))
+                c2 = n2C;
+
 
             if (c1.HasValue && c2.HasValue && c1.Value == c2.Value)
                 continue;
@@ -53,21 +118,29 @@ public class Day08
                         connectedToCluster[i] = c1.Value;
                         clusterContains[c1.Value].Add(i);
                     }
+                    clusterContains.Remove(c2.Value);
                 }
+
                 connectedToCluster[n2i] = c1.Value;
                 clusterContains[c1.Value].Add(n2i);
-            }else
+            }
+            else if (c2.HasValue)
             {
                 connectedToCluster[n1i] = c2.Value;
                 clusterContains[c2.Value].Add(n1i);
             }
-            pairs++;
+            else
+            {
+                var newClusterKey = clusterKey++;
+                connectedToCluster.Add(n1i, newClusterKey);
+                connectedToCluster.Add(n2i, newClusterKey);
+                clusterContains[newClusterKey] = [n1i, n2i];
+            }
 
-            connectedToCluster[n1i] = n2i;
-
+            if (clusterContains.Values.Select(cl => cl.Count).Max()
+                == boxes.Count)
+                return boxes[n1i].x * boxes[n2i].x;
         }
-
-        return 0;
     }
 
     private List<NodeDistance> FillDistances()
--- TestOf2025/Day08Tests.cs
+++ TestOf2025/Day08Tests.cs
@@ -9,21 +9,14 @@ public class Day08Tests
     public void Test1()
     {
         var d08 = new Day08("Day08".ReadTestLines());
-        Assert.That(d08.NodeThreeProduct(), Is.EqualTo(40));
-    }
-
-    [Test]
-    public void Test1_1()
-    {
-        var d08 = new Day08("Day08".ReadTestLines("1"));
-        Assert.That(d08.NodeThreeProduct(), Is.EqualTo(4));
+        Assert.That(d08.Part1(10), Is.EqualTo(40));
     }
 
     [Test]
     public void Real1()
     {
         var d08 = new Day08("Day08".ReadRealLines());
-        Assert.That(d08.NodeThreeProduct(), Is.EqualTo(269));
+        Assert.That(d08.Part1(1000), Is.EqualTo(115885));
     }
 
 
@@ -31,13 +24,13 @@ public class Day08Tests
     public void Test2()
     {
         var d08 = new Day08("Day08".ReadTestLines());
-        Assert.That(d08.NodeThreeProduct(true), Is.EqualTo(34));
+        Assert.That(d08.Part2(), Is.EqualTo(25272));
     }
 
     [Test]
     public void Real2()
     {
         var d08 = new Day08("Day08".ReadRealLines());
-        Assert.That(d08.NodeThreeProduct(true), Is.EqualTo(949));
+        Assert.That(d08.Part2(), Is.EqualTo(274150525));
     }
 }
