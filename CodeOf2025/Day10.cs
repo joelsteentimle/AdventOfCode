b2b5785421bdbb6a4e6e77d56633f5eea90fba14
joel <nordenjolt@gmail.com>
2025-12-10 21:44:45 +0100

    Day 10 isch

--- CodeOf2025/Day10.cs
+++ CodeOf2025/Day10.cs
@@ -1,32 +1,53 @@
+using System.Collections;
+
 namespace AoC2025;
 
 public class Day10(List<string> allData)
 {
-    private int[,] terrain;
+    // private int[,] terrain;
 
-    private record Config(bool[] lights, int[] switches, int[] cost);
+    private record Flipper(int pattern, int cost);
+
+    private record Config(int lights, Flipper[] flips);
 
     private Config[] configs = allData.Select(ReadConfigRow).ToArray();
 
     private static Config ReadConfigRow(string text)
     {
-        var lights = text[1..].TakeWhile(c  => c!= ']')
-            .Select(c => c == '#').ToArray();
+        var lights = text[1..].TakeWhile(c => c != ']').ToArray();
+        int targetLight = 0;
+        List<Flipper> flipps = [];
 
-        var switches = text.SkipWhile(c => c != ' ')
+        for (var i = 0; 0 < lights.Length; i++)
+            if (lights[i] == '#')
+                targetLight |= 2 ^ i;
+
+        var switchStrings = text.SkipWhile(c => c != ' ')
             .TakeWhile(c => c != '{')
             .ToString()
-            .Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
-            [1..^1]
-            .ToString()
-            .Split(',',StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
-            .Select(int.Parse).ToArray();
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
 
         var costs = text.SkipWhile(c => c != '{')
-            .ToString()[1..^1]
-            .Split(',',StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries )
+            .TakeWhile(c => c != '}')
+            .ToString()
+            ?.Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
             .Select(int.Parse).ToArray();
-        return new(lights, switches, costs);
+        
+        for(var i =0; i< switchStrings.Length; i++)
+        {
+            var sw = 0;
+            var switchPositions = switchStrings[i][1..^1]
+                .Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+                .Select(int.Parse).ToArray();
+
+            foreach (var switchPosition in switchPositions)
+                sw |= 2 ^ switchPosition;
+
+            flipps.Add(new(sw, costs[i]));
+        }
+
+
+        return new(targetLight, flipps.ToArray());
     }
 
 
