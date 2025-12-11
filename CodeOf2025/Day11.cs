56e311e49fbc0a819dd1027a5c03f81d3ef66d70
Joel Steen Timle <joelsteentimle@gmail.com>
2025-12-11 16:32:19 +0100

    Day 11 part

--- CodeOf2025/Day11.cs
+++ CodeOf2025/Day11.cs
@@ -2,32 +2,35 @@ namespace AoC2025;
 
 public class Day11
 {
-    // private readonly Dictionary<(long number, long nrBlinkings), long> MemoryOfNumbers = [];
-    // private readonly List<long> numbers = [];
-    // private readonly Dictionary<long, (long, long?)> stepping = [];
-
-    private int[] devices;
+    // private int[] devices;
     private Dictionary<string, int> deviceNamesNumbers =[];
     private int[][] devicesConnectedTo;
     private int youIndex;
     private int outIndex;
+    private int fftIndex;
+    private int dacIndex;
+    private int svrIndex;
+
 
     public Day11(List<string> allData)
     {
         var deviceNames = allData.Select(r => r.Split(':')[0])
             .ToArray();
 
-        devices = new int[deviceNames.Length];
-        devicesConnectedTo = new int[deviceNames.Length][];
-
         for (var i = 0; i < deviceNames.Length; i++)
         {
             deviceNamesNumbers[deviceNames[i]] = i;
             if(deviceNames[i] == "you") youIndex = i;
-            if(deviceNames[i] == "output") outIndex = i;
+            if(deviceNames[i] == "fft") fftIndex = i;
+            if(deviceNames[i] == "dac") dacIndex = i;
+            if(deviceNames[i] == "svr") svrIndex = i;
         }
 
-        devicesConnectedTo = allData
+        outIndex = deviceNames.Length;
+        deviceNamesNumbers["out"] = outIndex;
+
+
+       devicesConnectedTo = allData
             .Select(r => r.Split(':')[1]
                 .Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
                 .Select(dn => deviceNamesNumbers[dn])
@@ -36,18 +39,61 @@ public class Day11
             .ToArray();
     }
 
-    public long Part1()
-    {
-        int[] startPath = [youIndex];
-        Dictionary<int, int[]> pathsByLength = [];
-        pathsByLength[0] = startPath;
 
-        return 0;
+    private long CountValidPaths(Func<int[], bool> validPath, int startIndex)
+    {
+        int[] startPath = [startIndex];
+        Dictionary<int, List<int[]>> pathsByLength = [];
+        pathsByLength[0] = [startPath];
+
+        var pathsToEnd = 0;
+
+        for (var length = 0; length < 600; length++)
+        {
+            if (!pathsByLength.TryGetValue(length, out var currentPaths)
+                || currentPaths.Count == 0)
+                return pathsToEnd;
+
+            var nextLengthPaths = new List<int[]>();
+
+            foreach (var path in currentPaths)
+            {
+                if (validPath(path))
+                {
+                    pathsToEnd++;
+                    continue;
+                }
+
+                if (devicesConnectedTo.Length <= path.Last())
+                    continue;
+
+                foreach (var nextDevice in devicesConnectedTo[path.Last()])
+                {
+                    if (path.Contains(nextDevice))
+                        continue;
+
+                    var nextPath = new int[path.Length + 1];
+
+                    Array.Copy(path, nextPath, path.Length);
+                    nextPath[^1] = nextDevice;
+
+                    nextLengthPaths.Add(nextPath);
+
+                }
+            }
+            pathsByLength[length + 1] = nextLengthPaths;
+        }
+
+        return pathsToEnd;
     }
 
-    public long Part2()
-    {
-        return 0;
-    }
+    private bool ValidPathPart1(int[] path) => path.Last() == outIndex;
+
+    public long Part1() => CountValidPaths(ValidPathPart1, youIndex);
+
+    private bool ValidPathPart2(int[] path) =>
+        path.Contains(fftIndex) && path.Contains(dacIndex) && path.Last() == outIndex;
+
+    public long Part2()=> CountValidPaths(ValidPathPart2, svrIndex);
 
 }
--- TestOf2025/Day11Tests.cs
+++ TestOf2025/Day11Tests.cs
@@ -16,15 +16,15 @@ public class Day11Tests
     public void Real1()
     {
         var d11 = new Day11("Day11".ReadRealLines());
-        Assert.That(d11.Part1(), Is.EqualTo(217443));
+        Assert.That(d11.Part1(), Is.EqualTo(764));
     }
 
 
     [Test]
-    public void Test2_smallBlink()
+    public void Test2()
     {
-        var d11 = new Day11("Day11".ReadTestLines());
-        Assert.That(d11.Part1(6), Is.EqualTo(22));
+        var d11 = new Day11("Day11".ReadTestLines("2"));
+        Assert.That(d11.Part2(), Is.EqualTo(2));
     }
 
     [Test]
